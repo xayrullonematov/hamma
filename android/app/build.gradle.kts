06276782b710da0a64fee89387f541a06b6a8db7
VIPStone <vipstoneagex@gmail.com>
2026-04-24 06:38:42 -0400

    Fix coreLibraryDesugaringEnabled property name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
